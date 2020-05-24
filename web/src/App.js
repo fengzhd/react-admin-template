1728c33d5dcd4d06276bca674edfd2146c7c8313
fzdMacPro <fzdMacPro@qq.com>
2020-05-25 00:04:13 +0800

    添加404页面路由

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Default from 'layouts/Default';
 import Login from 'pages/Login'
+import Notfind from 'pages/Notfind'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route exact path="/" render={() => <Redirect to="/index" />} />
       <Switch>
         <Route path='/login' component={Login}></Route>
+        <Route path='/404' component={Notfind}></Route>
         <Route path='/' component={Default}></Route>
       </Switch>
     </BrowserRouter>
