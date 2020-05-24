ed3fb515cf6f38b7a0e51bef5f128e6333b1818e
fzdMacPro <fzdMacPro@qq.com>
2020-05-24 23:35:47 +0800

    redux案例完善,添加页面state数据结构显示

--- web/src/pages/Redux.jsx
+++ web/src/pages/Redux.jsx
@@ -1,22 +1,42 @@
 import React, { Component } from 'react'
 import Counter from 'components/redux/Counter'
 import NewsList from 'components/redux/NewsList'
-import { Divider } from 'antd';
+import { Divider, Row, Col } from 'antd';
+import { connect } from 'react-redux'
+import ReactJson from 'react-json-view'
 
 
 export class ReduxDemo extends Component {
   render() {
     return (
-      <div>
-        {/* 计数器例子 */}
-        <Counter />
-        <Divider />
-        {/* 异步actions, 获取列表 */}
-        <NewsList />
-      </div>
+      <Row>
+        <Col span={12}>
+          <div>
+            {/* 计数器例子 */}
+            <h1>触发同步action</h1>
+            <Counter />
+            <Divider />
+            {/* 异步actions, 获取列表 */}
+            <h1>触发异步action</h1>
+            <NewsList />
+          </div>
+        </Col>
+        <Col span={12}>
+          <h1>state结构</h1>
+          <div className="h-500 o-y-scroll">
+            <ReactJson src={this.props.state} />
+          </div>
+        </Col>
+      </Row>
+
     )
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    state
+  }
+}
 
-export default ReduxDemo
+export default connect(mapStateToProps)(ReduxDemo)
