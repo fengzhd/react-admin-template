ffa0621a39b307d1a94815e32dabfc1747af0c66
fzdMacPro <fzdMacPro@qq.com>
2020-05-26 22:56:04 +0800

    路由改造

--- web/src/layouts/Routers.jsx
+++ web/src/layouts/Routers.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Suspense, lazy } from 'react'
+import React, { Component } from 'react'
 import { Route, Redirect } from 'react-router-dom';
 
 import menuList from './router'
@@ -10,26 +10,17 @@ export class Routers extends Component {
         {
           menuList.map(item => {
             return (
-              // <Route key={item.key} path={item.path} component={require('pages/' + item.component).default} />
-              <Suspense fallback={<div>Loading...</div>}>
-                {
-                  isLogin ? 
-                  (
-                    <Route key={item.key} path={item.path} component={item.component} />
-                  )
-                  : 
-                  (
-                    <Redirect to={{ pathname: "/login"}}/>
-                  )
-                }
-              </Suspense>
+              isLogin ?
+                (
+                  <Route key={item.key} path={item.path} component={item.component} />
+                )
+                :
+                (
+                  <Redirect key={-1} to={{ pathname: "/login" }} />
+                )
             )
           })
         }
-        {/* <Route path="/index" component={Index} />
-        <Route path="/permission" component={Permission} />
-        <Route path="/redux" component={Redux} />
-        <Route path="/hook" component={Hook} /> */}
       </div>
     )
   }
